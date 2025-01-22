cecfe82182330a925fca09d3fe365cb0ade6fe6d
SuzukiShuntarou <da8801593@gmail.com>
2025-01-22 23:41:34 +0900

    Rewardの作成機能、表示機能を追加

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,13 +1,33 @@
 class RewardsController < ApplicationController
   def index
+    @rewards = Reward.order(completiondate: :asc).page(params[:page]).per(3)
   end
 
   def show
+    @reward = Reward.find(params[:id])
   end
 
   def new
+    @reward = current_user.rewards.new
   end
 
   def edit
+  end
+
+  def create
+    @reward = current_user.rewards.new(reward_params)
+
+    if @reward.save
+      redirect_to reward_url(@reward), notice: 'ご褒美の登録に成功！'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def reward_params
+    params.require(:reward).permit(:completiondate ,:content ,:location)
   end
 end
