46cdd79c0bfc5bcb2e4878bf0dd16d005cc7236b
SuzukiShuntarou <da8801593@gmail.com>
2025-01-23 00:44:26 +0900

    表示はされないが一旦commit

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,7 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="modal"
+import { Controller } from "@hotwired/stimulus";
+import { Modal } from "bootstrap";
 export default class extends Controller {
   connect() {
+    this.modal = new Modal(this.element);
+    this.modal.show();
+  }
+  close(event) {
+    if (event.detail.success) {
+      this.modal.hide();
+    }
   }
 }
