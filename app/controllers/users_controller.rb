0ab4bf8e8a9c65b7d3fb3e14740fe68069d0c316
SuzukiShuntarou <da8801593@gmail.com>
2025-01-22 23:32:13 +0900

    ページネーションを３人まで、UserをID昇順で並ぶ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(id: :desc).page(params[:page])
-  end
+    @users = User.order(id: :asc).page(params[:page]).per(3)  end
 
   def show
     @user = User.find(params[:id])
