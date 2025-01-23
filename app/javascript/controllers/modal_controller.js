da1395105109a59effd9e21d770dde04988b8e9b
SuzukiShuntarou <da8801593@gmail.com>
2025-01-23 12:09:12 +0900

    モーダルが表示される

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
-import { Modal } from "bootstrap";
+// import { Modal } from "bootstrap";
 export default class extends Controller {
   connect() {
-    this.modal = new Modal(this.element);
+    // this.modal = new Modal(this.element);
+    this.modal = new bootstrap.Modal(this.element);
     this.modal.show();
   }
   close(event) {
