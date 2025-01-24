86324aa0ec6c86fb15be136c025db55a4967c432
SuzukiShuntarou <da8801593@gmail.com>
2025-01-24 22:39:16 +0900

    目標モデルのモーダル化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # user_path(resource)
-    rewards_path(resource)
+    # rewards_path(resource)
+    treats_path(resource)
   end
 
   def after_sign_up_path_for(resource)
